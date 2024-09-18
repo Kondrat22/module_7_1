914e98b4a86692fce5526fb9747e012a133b3441
Kondrat22 <kondrat_va@bk.ru>
2024-09-18 22:25:17 +0300

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -7,10 +7,12 @@ class Product:
     def __str__(self):
         return f'{self.name}, {self.weight}, {self.category}'
 
+
 class Shop:
-    __file_name = 'products.txt'
 
     def __init__(self):
+
+        self.__file_name = 'products.txt'
 
         open(self.__file_name, 'a').close()
 
@@ -20,31 +22,14 @@ class Shop:
         file.close()
         return products
 
-    def add(self, *products):
-        existing_products = {}
-        file_content = self.get_products()
-        lines = []
-        index = 0
-
-        while index < len(file_content):
-            line_end = file_content.find('\n', index)
-            if line_end == -1:
-                lines.append(file_content[index:])
-                break
-            lines.append(file_content[index:line_end])
-            index = line_end + 1
-
-        for line in lines:
-            if line:
-                existing_products[line[:line.find(',')]] = True
-
-        for product in products:
-            product_name = product.name
-            if product_name in existing_products:
-                print(f'Продукт {product} уже есть в магазине')
+    def add(self, *products: Product):
+        for i in products:
+            if i.name in self.get_products():
+                print(f'Продукт {i.name} уже есть в магазине')
+                continue
             else:
                 file = open(self.__file_name, 'a')
-                file.write(f"{product}\n")
+                file.write(f'{str(i)}\n')
                 file.close()
 
 s1 = Shop()
